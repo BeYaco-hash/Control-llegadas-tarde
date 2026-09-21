0bb575d4472f937e009f319918d80ca983ab72e8
BeYaco-hash <jacobocast26@gmail.com>
2026-09-21 10:29:07 -0500

    feature(arquitectura):
    creación base de frontend y backend

---
+++ frontend-control-llegadas-tardes/vite.config.js
@@ -0,0 +1,35 @@
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'frontend-control-llegadas-tardes',
+      short_name: 'frontend',
+      description: 'aplicacion para monitorear las llegadas tarde del colegio',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
